216c02af47db064261405e4f53a53235038c732b
shekharfsd <77474859+shekharfsd@users.noreply.github.com>
2024-07-31 21:15:19 +0530

    Add files via upload

---
+++ Week-3/30th July 2024/Handson/Parameterized Test/RainbowJewellers.java
@@ -0,0 +1,28 @@
+package com.mphasis.parameterized;
+
+import java.util.Scanner;
+
+
+
+public class RainbowJewellers {
+
+	public static void main(String[] args) {
+		// CODE SKELETON - VALIDATION STARTS
+		// DO NOT CHANGE THIS CODE
+	//	SkeletonValidator validator = new SkeletonValidator();
+		// CODE SKELETON - VALIDATION ENDS
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the gold in grams");
+		double grams = Double.parseDouble(scanner.nextLine());
+		
+		RainbowJewellersService rainbowJewellersService = new RainbowJewellersService();
+		
+		double totalPriceOfGold = rainbowJewellersService.calculategoldprice(grams);
+		
+		System.out.println("Total Price of the Gold is: " + totalPriceOfGold);
+	}
+
+}
+
